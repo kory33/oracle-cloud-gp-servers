225ffae412839b3f3cf3427c1ec831ea9393e169
Ryosuke Kondo <korygm33@gmail.com>
2021-06-13 10:39:35 +0000

    remove prompts by server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-
 set -e
+
+# setup git
+git config --global user.email "korygm33@gmail.com"
+git config --global user.name "Ryosuke Kondo"
 
 sudo apt update && sudo apt upgrade -y
 
 # region install toolchains
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get remove -y docker docker-engine docker.io containerd runc || true
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 # endregion
 
 # region install docker
+# skip prompts (see https://stackoverflow.com/questions/7519375/how-to-automatically-overwrite-the-output-file-when-running-gpg-i-e-without)
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-  | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo "deb \
       [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
@@ -19,10 +23,10 @@ echo "deb \
       $(lsb_release -cs) stable" \
       | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 # endregion
 
 # region install docker-compose
 sudo apt install python3-pip
-sudo pip install docker-compose
+yes | sudo pip install docker-compose
 # endregion
