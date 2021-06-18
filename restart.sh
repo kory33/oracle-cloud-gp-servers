8721ec393a60565c9b372883c1ec2fca83e33b2f
Ryosuke Kondo <korygm33@gmail.com>
2021-06-18 20:51:05 +0000

    stop requiring restart.sh be invoked as a root

--- restart.sh
+++ restart.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root. Exiting..." 
-   exit 1
-fi
-
-docker-compose -f ./docker-compose.yml down
-docker-compose -f ./docker-compose.yml pull
-docker-compose -f ./docker-compose.yml up --force-recreate --build
+sudo docker-compose -f ./docker-compose.yml down
+sudo docker-compose -f ./docker-compose.yml pull
+sudo docker-compose -f ./docker-compose.yml up --force-recreate --build
