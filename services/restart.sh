c0716b9e6055c71735b5f0cf5d4bbfe1999b85cc
Ryosuke Kondo <korygm33@gmail.com>
2021-06-18 21:48:25 +0000

    restructure project

--- services/restart.sh
+++ services/restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 sudo docker-compose -f ./docker-compose.yml down
 sudo docker-compose -f ./docker-compose.yml pull
 sudo docker-compose -f ./docker-compose.yml up --force-recreate --build
